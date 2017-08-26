d390422f71ae6f39e2d444b6bbe42d9eaf572fc6
Allen Anderson <dande313@byu.net>
2017-08-26 12:00:24 -0400

    Added Introduction & basic HTML scrape

--- lib/cli_lookup.rb
+++ lib/cli_lookup.rb
@@ -10,5 +10,26 @@ class Lookup
     	puts "\e[H\e[2J"
     end
 
+	def introduction
+		puts ""
+	    puts "************* Top 10 Highest Rated Board Games *************"
+	    puts "                           v.01                             "
+	    puts "                     by Allen Anderson                      "
+	    puts ""
+	    puts ""
+	end
+
+
+
+
+	def run
+		clear_screen
+		introduction
+		data = Scraper.scrape_index_page("https://boardgamegeek.com/browse/boardgame") 
+		puts "Here is the data you requested:"
+		puts data
+		exit
+	end
+
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,20 @@ class Scraper
   def initialize
   end
 
+  def self.scrape_index_page(index_url)
+    info_arr = []
+    index_html = Nokogiri::HTML(open(index_url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+    names = index_html.search("h4[itemprop='name'] a[itemprop='url']")
+
+    #index_html.css("").each do |student|
+      #info = {
+ 
+      #}
+      #info_arr << info
+    #end
+
+    return index_html
+  end
 
 
 
